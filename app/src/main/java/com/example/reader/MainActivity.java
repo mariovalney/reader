8e261c7352a8ca3a90ec441c62231d488b5913f9
Mário Valney <mariovalney@gmail.com>
2015-08-06 02:32:15 -0300

    Aula 8: Vish... deu pau

--- app/src/main/java/com/example/reader/MainActivity.java
+++ app/src/main/java/com/example/reader/MainActivity.java
@@ -12,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -64,9 +70,6 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
-            // Aprendendo a escrever um LOG...
-            Log.e(LOG_TAG, "Não se preocupe, não é um erro mesmo, apenas um teste!");
-
             // Criando uma array falsa de strings com os títulos das nossas Aulas
             String[] data = {
                     "Aula 1: Getting Start!",
@@ -94,6 +97,59 @@ public class MainActivity extends ActionBarActivity {
             // Cria uma referência para a ListView
             ListView listView = (ListView) rootView.findViewById(R.id.list_last_posts);
             listView.setAdapter(listOfLastPostsAdapter);
+
+            // As variáveis da conexão devem ser declaradas fora do try/catch
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Irá conter a resposta como uma string
+            String responseJson = null;
+
+            try {
+                // Constrói a URL a que iremos enviar o Request
+                // Mais detalhes em: http://api.jangal.com.br/reader
+                URL url = new URL("http://api.jangal.com.br/reader/v1/categorias");
+
+                // Cria o Request (GET) e abre a conexão
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Lê a o input stream (entrada) como uma string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Se for nulo, mantemos a resposta como "null"
+                    responseJson = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                if (buffer.length() == 0) {
+                    // Como o buffer está vazio, a resposta também estará
+                    responseJson = null;
+                }
+
+                // Atribui o buffer como string à variável que armazena a resposta
+                responseJson = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Erro: ", e);
+                // Como houve uma excessão, deixamos a resposta como null
+                responseJson = null;
+            } finally{
+                if (urlConnection != null) {
+                    // Se houve conexão, fechamos
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    // Se o reader não estiver nulo ainda, tentamos fechar
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        // Se houver excessão ao fechar, escrevemos um Log
+                        Log.e(LOG_TAG, "Erro fechando o (BufferedReader) reader", e);
+                    }
+                }
+            }
 
             // Retornamos tudo
             return rootView;
